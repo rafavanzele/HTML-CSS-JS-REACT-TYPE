0f659648e87a8e4abaf0b9dcffa753972d7ff4b1
rafavanzele <rafa_vanzele@hotmail.com>
2024-03-28 17:23:48 -0300

    + js

--- JAVASCRIPT/OPERADORES/js/scripts.js
+++ JAVASCRIPT/OPERADORES/js/scripts.js
@@ -1,3 +1,18 @@
 // 1 - NUMBER
 console.log(37);
 console.log(typeof 37); //typeof+espaço mostra no console a prova real do tipo de dado
+
+// 2 - Special Numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(2 * 'rvg');
+console.log(typeof NaN);
+
+// 3 - Strings
+console.log("Um texto em aspas duplas");
+console.log('Outro texto em aspas simples');
+console.log('Texto em \n duas linhas'); // \n quebra a linha do texto
+console.log('Espaçamento \t em tab'); // \t da um espaçamento de tab no texto
+
+// 4 - Template Strings (interpolação)
+console.log(`A soma de 2 + 2 é igual a ${2+2}`);

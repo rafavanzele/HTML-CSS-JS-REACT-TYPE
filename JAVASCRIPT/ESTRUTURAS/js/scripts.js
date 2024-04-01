74ecb216aa37e694115fb409bb8e79542903eadd
rafavanzele <rafa_vanzele@hotmail.com>
2024-04-01 11:04:40 -0300

    mais sobre estruturas

---
+++ JAVASCRIPT/ESTRUTURAS/js/scripts.js
@@ -0,0 +1,18 @@
+// 1 - Variáveis
+// a - Let (a variável pode ser alterada ao longo do programa, atribuir diversos valores)
+let nome = "Rafael"
+console.log(nome)
+
+nome = 'Rafael Vanzele'
+console.log(nome)
+
+// b - Const (a variável não será alterada, ela é CONSTANTE, ou seja, só assumirá o primeiro 'valor' que lhe foi atribuido. NA MAIORIA DOS PROJETOS É USADO CONST PORQUE NÃO SE COSTUMA MUDAR MUITO O VALOR DAS VARIÁVEIS)
+const idade = 37
+console.log(idade)
+
+//idade = 38
+//console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+

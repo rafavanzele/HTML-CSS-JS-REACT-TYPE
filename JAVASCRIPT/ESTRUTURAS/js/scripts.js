ea5c4084fef8f3f107fa8f1cfd159851174371da
rafavanzele <rafa_vanzele@hotmail.com>
2024-04-01 17:58:55 -0300

    funções, estruturas de controle,

--- JAVASCRIPT/ESTRUTURAS/js/scripts.js
+++ JAVASCRIPT/ESTRUTURAS/js/scripts.js
@@ -16,3 +16,83 @@ console.log(idade)
 console.log(typeof nome)
 console.log(typeof idade)
 
+
+
+//FUNÇÕES(são blocos de códigos que podem ser reaproveitados ao longo do programa)
+
+// 1 - Função prompt (abre uma caixinha de "verificação" e o site (programa) so volta a funcionar quando validar essa caixinha. É uma função raramente utilizada)
+//const age = prompt('Digite a sua idade')
+//console.log(`Sua idade é ${age} anos`)
+
+// 2 - Função alert (igual o prompt)
+//alert('Testando')
+
+// 3 - Função math
+console.log(Math.max(1, 3, 56, 43, 21)) //método que encontra o maior valor
+
+console.log(Math.floor(14,25)) //método arredonda pra baixo
+
+console.log(Math.ceil(14,25))  //método que arredonda pra cima
+
+// 4 - Função console
+console.log('teste!')
+
+console.error('erro!')
+
+console.warn('aviso!')
+
+
+// IF
+const m = 10
+
+if(m > 5) {
+    console.log('M é maior que 5!')
+}
+
+const user = 'João'
+
+if(user === 'João') {
+    console.log('Bom dia João!')
+}
+
+//ELSE
+const loggedIn = false
+
+if(loggedIn) {
+    console.log('Está autenticado')
+} else {
+    console.log('Não está autenticado')
+}
+
+
+const q = 10
+const w = 15
+
+if(q > 5 && w > 16) {
+    console.log('Números mais altos')
+} else {
+    console.log('Os números não sao mais altos')
+}
+
+
+// ELSE IF 
+if(1 > 2) {
+    console.log('Teste 1')
+} else if(3 > 4) {
+    console.log('Teste 2')
+} else if(1 < 5) {
+    console.log('Agora sim!')
+}
+
+
+const userName = 'Rafael'
+const userAge = 37
+
+if(userName === 'Jonas') {
+    console.log('Bem vindo Jonas!')
+} else if(userName === 'Rafael' && userAge === 37) {
+    console.log('Olá Rafael, você tem 37 anos')
+} else {
+    console.log('Nenhuma condição aceita!')
+}
+

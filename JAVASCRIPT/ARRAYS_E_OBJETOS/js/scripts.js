90b0ad0fe2ec5791cd368d535e451f95b8d3461b
rafavanzele <rafa_vanzele@hotmail.com>
2024-04-03 18:01:35 -0300

    mais sobre arrays e objetos

--- JAVASCRIPT/ARRAYS_E_OBJETOS/js/scripts.js
+++ JAVASCRIPT/ARRAYS_E_OBJETOS/js/scripts.js
@@ -33,3 +33,54 @@ console.log(text.indexOf('x')) //método para identificar o indice de um dado de
 console.log(typeof text.toUpperCase)
 
 
+// 5 - OBJETOS (Objects literals) é declarado em uma variável, possui apenas propriedades e métodos e fica em um bloco de chaves
+const person = {
+    name: 'Rafael',
+    age: 37,
+    genre: 'male'
+}
+
+console.log(person) //imprime todo o conteudo do objeto
+console.log(person.name) //imprime uma propriedade específica dentro do objeto
+console.log(person.genre.length) //imprime o 'tamanho' de determinada propriedade do objeto
+console.log(typeof person) //imprime o tipo de dado que é
+
+
+// 6 - CRIANDO E DELETANDO PROPRIEDADES
+// 6.a. Criando propriedade:
+const car = {
+    brand: 'VW',
+    model: 'Fox',
+    color: 'preto',
+    km: 10000
+}
+
+console.log(car)
+
+car.doors = 2 //para criar propriedades num objeto, basta usar o 'nomedoobjeto.proriedade'
+console.log(car)
+
+// 6.b. Deletando propriedade:
+const tv = {
+    color: 'preto',
+    size: 60,
+    smart: true
+}
+
+console.log(tv)
+
+delete tv.smart //para deletar  uma propriedade basta usar o 'delete' + nome da propriedade
+
+console.log(tv)
+
+
+
+// 7 - MÉTODO KEYS (PARA OBJETOS) podemos verificar as propriedades de um objeto usando o método KEYS
+// nesse exemplo vou testar no objeto car (que criei logo acima)
+console.log(Object.keys(car))
+
+// nesse exemplo vou testar no objeto tv (que criei logo acima)
+console.log(Object.keys(tv))
+
+// nesse exemplo vou testar o método ENTRIES para verificar as chaves e os valores do objeto
+console.log(Object.entries(car))

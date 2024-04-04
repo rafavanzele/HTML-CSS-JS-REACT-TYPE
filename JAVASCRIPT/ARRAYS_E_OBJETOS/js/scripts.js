cac84995426f5ba9813b7c324603edb149ce15eb
rafavanzele <rafa_vanzele@hotmail.com>
2024-04-04 11:44:47 -0300

    mais sobre arrays e objetos

--- JAVASCRIPT/ARRAYS_E_OBJETOS/js/scripts.js
+++ JAVASCRIPT/ARRAYS_E_OBJETOS/js/scripts.js
@@ -84,3 +84,74 @@ console.log(Object.keys(tv))
 
 // nesse exemplo vou testar o método ENTRIES para verificar as chaves e os valores do objeto
 console.log(Object.entries(car))
+
+
+
+// 8 - MUTAÇÃO (alteração de um objeto dentro de outro objeto/ não é muito recomendado, basta conferir o pq no exemplo abaixo)
+const a = {
+    name: 'Rafael',
+}
+
+const b = a
+
+console.log(a)
+console.log(b)
+
+a.age = 37
+console.log(a)
+console.log(b)
+
+
+
+// 9 - LOOP EM ARRAYS
+const users = ['Rafael', 'Pedro', 'João', 'José']
+
+for (i = 0; i < users.length; i++) {
+    console.log(`Listando users com um loop: ${users[i]}`)
+}
+
+
+
+// 10 - MÉTODO PUSH E POP DE ARRAY (com push podemos add um elemento ao fim do array, e o pop podemos remover o ultimo elemento do array)
+//10.a
+const array = ['a', 'b', 'c']
+
+array.push('d')
+console.log(array)
+
+//10.b
+array.pop()
+console.log(array)
+
+//10.b.1 imprimindo o item removido
+const itemRemovido = array.pop()
+console.log(itemRemovido)
+console.log(array)
+
+//10.b.2 add varios elementos ao mesmo tempo no array
+array.push('x', 'y', 'z')
+console.log(array)
+
+
+
+// 11 - SHIFT E UNSHIFT (REMOVE E ADICIONA, RESPECTIVAMENTE, ELEMENTOS AO 'INICIO' DO ARRAY)
+const array2 = ['e', 'f', 'g', 'h']
+const letter = array2.shift() //resgatando (e talvez imprimindo) o elemento removido
+
+console.log(letter)
+console.log(array2)
+
+array2.unshift('p', 'q', 'r')
+console.log(array2)
+
+
+
+// 12 - INDEXOF E LASTINDEXOF (semelhante ao que foi mostrado antes, o indexOf mostra o indice de um determinado elemento de um array. o lastIndexOf faz o mesmo processo mas lendo o array do fim pro começo; é utilizado quando há elementos 'repetidos')
+const myElements = ['laranja', 'uva', 'morango', 'abacaxi', 'limão', 'uva']
+
+console.log(myElements.indexOf('laranja'))
+console.log(myElements.indexOf('uva'))
+
+console.log(myElements.lastIndexOf('uva'))
+//obs.: quando o elemento não existe, em ambos os casos o índice aparece como -1. Veja exemplo:
+console.log(myElements.indexOf('açaí'))

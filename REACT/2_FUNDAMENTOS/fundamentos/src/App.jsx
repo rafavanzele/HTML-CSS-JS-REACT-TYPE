dbba5723eb062ff0b2d116e5b4548d4302e5083f
rafavanzele <rafa_vanzele@hotmail.com>
2024-05-28 11:44:27 -0300

    2_FUNDAMENTOS_REACT

---
+++ REACT/2_FUNDAMENTOS/fundamentos/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import FirstComponent from './components/FirstComponent'
+
+function App() {
+
+  return (
+    <>
+      <h1>2_FUNDAMENTOS</h1>
+      <FirstComponent/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ REACT/2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h2>2_Meu Primeiro Componente</h2>
+        </div>
+    )
+}
+
+export default FirstComponent

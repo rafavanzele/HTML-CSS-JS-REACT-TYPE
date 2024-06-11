578db32aaacfbb12c30ac1ad64ee7b930dba6912
rafavanzele <rafa_vanzele@hotmail.com>
2024-06-11 11:45:59 -0300

    GET e POST

--- REACT/6_HTTP_REACT/http_react/src/App.jsx
+++ REACT/6_HTTP_REACT/http_react/src/App.jsx
@@ -23,6 +23,36 @@ function App() {
   }, []) //como vamos regarregar os dados apenas uma vez, o segundo argumento é um array vazio
 
   
+
+  // 2 ENVIANDO DADOS
+  // selecionando e guardando os dados que vao ser carregados
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+
+
+    // carregamento de dados dinamico
+    const addedProduct = await res.json()
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct])
+  }
+
+
   return (
     <div>
       <h1>HTTP em React</h1>
@@ -35,6 +65,24 @@ function App() {
           </li>
         ))}
       </ul>
+
+
+      {/* 2 adicionando dados */}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Nome:</span>
+            <input type="text" value={name} onChange={(e) => setName(e. target.value)}/>
+          </label>
+
+          <label>
+            <span>Preço:</span>
+            <input type="text" value={price} onChange={(e) => setPrice(e. target.value)}/>
+          </label>
+
+          <input type="submit" value='Enviar' />
+        </form>
+    </div>
     </div>
   )
 }
